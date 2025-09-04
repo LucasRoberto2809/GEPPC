552382e9f11564fa704c995567fc1d11d64c3bd6
LucasRoberto2809 <lrgoncalves47@gmail.com>
2025-09-04 10:37:22 -0300

    adição de script e anotações

---
+++ NOTAS.md
@@ -0,0 +1,5 @@
+## Inteiro
+- não gera atualmente não gera negativos
+
+## Geral
+- automatizar criação de casos de teste
---
+++ make.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Compila o arquivo .y usando o bison e gera o parser e o arquivo de cabeçalho (.h)
+bison -d gerador.y
+
+# Compila o arquivo .l usando o flex e gera o analisador léxico
+flex gerador.l
+
+# Compila os arquivos C gerados pelo bison e flex e cria o executável
+gcc gerador.tab.c lex.yy.c -o gerador -lm
+
+# Exibe uma mensagem de sucesso
+echo "Compilação concluída! O executável 'gerador' foi criado."
